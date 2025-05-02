a8a680ba92f1ecadaa83d896b36e1fee7b01bcef
Abdullah Sayed <devabdullahsy@gmail.com>
2025-05-03 00:42:09 +0300

    delete all JS's task and start a new Learn JS task

---
+++ learn-javascript-with-ChatGPT/script.js
@@ -0,0 +1,20 @@
+// Data Types
+/*
+ * String -> "Abdullah Sayed"
+ * Number -> 123
+ * Boolean -> true / false
+ * Null -> قيمة فارغة
+ * Undefined -> متغير غير مُعرّف بعد
+ * Object, Array -> --
+ */
+
+// My Info ..
+const myName = "Abdullah Sayed";
+const myBirthYear = 1994;
+let isLearning = true;
+
+console.log(myName, myBirthYear, isLearning); // Abdullah Sayed 1994 true
+console.log(typeof myName, typeof myBirthYear, typeof isLearning); // string number boolean
+console.log("Name: " + myName); // Name: Abdullah Sayed
+console.log("Age: " + myBirthYear); // Age: 1994
+console.log("Learning JavaScript: " + isLearning); // Learning JavaScript: true
